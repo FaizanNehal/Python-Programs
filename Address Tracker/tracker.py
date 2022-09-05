df9d61e428e630c31a7af408a79619d8c05fb6d8
Faizan Nehal <45764507+FaizanNehal@users.noreply.github.com>
2022-09-06 02:22:37 +0500

    Add files via upload

--- Address Tracker/tracker.py
+++ Address Tracker/tracker.py
@@ -9,16 +9,20 @@ from audioop import add
 addresses= [line.rstrip() for line in open('./address.txt')]
 etherscanAPI ="GCQP7EA7N2F7GAIQ2JW5HVYDTZ9ZSE5VRS"
 addressToTrack=""
-endpoint="https://api.etherscan.io/api?module=account&action=balance&address={}&tag=latest&apikey={}"
-
+endpointBalance="https://api.etherscan.io/api?module=account&action=balance&address={}&tag=latest&apikey={}"
+endpointInternal="https://api.etherscan.io/api?module=account&action=txlistinternal&address={}&startblock=0&endblock=2702578&page=1&offset=10&sort=asc&apikey={}"
+endpoints=[] #CREATE A NESTED ARRAY WITH EACH ELEMENT CONTAINING TYPE OF TRANACTION AND ENDPOINT
+                #CREATE ATLEAST 5 TYPES OF DATA
+                #SAVE THE RESULT IN ANOTHER NESTED ARRAY
+                # IN THE END SAVE IT IN THE FILE PROPERLY
 
 
 def tracker():
     counter=0
     for index,addr in enumerate(addresses):
         print(addr)
-        historyReq= requests.get(endpoint.format(addr,etherscanAPI))
-        history= historyReq.text
+        historyReq= requests.get(endpointBalance.format(addr,etherscanAPI))
+        history= historyReq.json().get("result")
         counter+=1
         with open("transactions.txt","a") as file:
             file.write("ADDRESS # {}: {}\n".format(index+1,addr))
