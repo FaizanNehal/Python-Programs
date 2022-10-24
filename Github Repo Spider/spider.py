9ddd5961730b7c5025666ccccf860cd4aa9861b8
Faizan Nehal <45764507+FaizanNehal@users.noreply.github.com>
2022-10-24 17:28:03 +0500

    Add files via upload

--- Github Repo Spider/spider.py
+++ Github Repo Spider/spider.py
@@ -1,9 +1,14 @@
+from os import link
 import requests,re
 
 links=[]
 pages=[]
+visited=[]
+report=[]
+
 githubRepo="https://github.com/orgs/Unipilot/repositories"
 base="https://github.com"
+ignoreForks='<span class="text-small lh-condensed-ultra no-wrap mt-1" data-repository-hovercards-enabled>       forked from'
 
 def main():
     repository=requests.get(githubRepo)
@@ -14,15 +19,43 @@ def main():
     for elements in links:
         print(elements)
 
-    for ix,x in enumerate(links):
+    while len(links)>0:
+
+        x = links[0]
+    #for ix,x in enumerate(links):
+    
         print(base+x)
         recursiveCalls=requests.get(base+x)
+        
         recursion=True
         #print(recursiveCalls.text)
         # with open("./test.txt","a") as file:
         #     file.write("{}".format(recursiveCalls.text))
         regexR=re.findall('<a class="js-navigation-open Link--primary(.*?)</a>',recursiveCalls.text)
-        print(len(regexR))
+
+        findFork=re.findall('forked from',recursiveCalls.text)
+        print(findFork)
+        if len(findFork) >0:
+            links.pop(0)
+            continue
+
+        print("-----------------------")
+        print("-----------------------")
+
+        temp=[]
+        for elements in regexR:
+            temp.append(re.search('href="(.+)"',elements).group(1))
+        for elements in temp:
+            if (elements not in visited) and (elements not in links):
+                
+                
+                links.append(elements)
+                print("NOT NOT NOT")
+
+        y = links.pop(0)
+        visited.append(y)
+
+
            
 
 if __name__=="__main__":
